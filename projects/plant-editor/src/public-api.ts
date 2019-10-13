25a7c9b341950c046b5ce16f317a25129b4c19a2
Dedalus Max <dedalus123@gmail.com>
2019-10-13 08:58:27 +0200

    del

--- projects/plant-editor/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of plant-editor
- */
-
-export * from './lib/plant-editor.service';
-export * from './lib/plant-editor.component';
-export * from './lib/plant-editor.module';
