0832107ac72a5a2fce0dfeba7e39488ad43c1ab4
Ratko Ćosić <ratko_cosic@hotmail.com>
2019-10-20 11:45:23 +0200

    device rebranded

--- src/app/devices/device/device.component.ts
+++ src/app/devices/device/device.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class DeviceComponent implements OnInit {
 
   @Input() partno: number;
-  @Input() manufacturer = 'Sauter';
+  @Input() platform = 'Azure Sphere';
 
   constructor() { }
 
